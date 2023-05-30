292106513a35d2a06cac9d41d7ccf63a1f650dfd
Auraxc <abeats@live.com>
2023-05-30 14:10:56 +0800

    添加 /static/ 路由，实现文件服务，修改模板以使用静态文件

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -6,11 +6,20 @@ import (
 )
 
 func main() {
-	// 注册两个新的 handler 方法，并且指定路由
-	// 方法和之前注册 home 路由是一致的：实现路由方法，注册路由
-	// Register the two new handler functions and corresponding URL patterns with
-	// the servemux, in exactly the same way that we did before.
 	mux := http.NewServeMux()
+
+	// Create a file server which serves files out of the "./ui/static" directory.
+	// Note that the path given to the http.Dir function is relative to the project
+	// directory root.
+	// 创建一个文件服务，指定 "./ui/static/" 为根目录
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+
+	// Use the mux.Handle() function to register the file server as the handler for
+	// all URL paths that start with "/static/". For matching paths, we strip the
+	// "/static" prefix before the request reaches the file server.
+	// 用 mux.Handle() 注册文件服务，以 /static/ 开头的路径都将路由到文件服务
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
