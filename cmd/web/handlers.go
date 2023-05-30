39225b00266bc333e42e5f125647cb086485604c
Auraxc <abeats@live.com>
2023-05-30 17:12:20 +0800

    删掉不需要的注释

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -18,23 +18,12 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Initialize a slice containing the paths to the two files. It's important
-	// to note that the file containing our base template must be the *first*
-	// file in the slice.
-	// 初始化一个包含两个文件路径的切片，注意：定义的基础模板必须在第一个文件的位置
 	files := []string{
 		"./ui/html/base.tmpl",
 		"./ui/html/partials/nav.tmpl",
 		"./ui/html/pages/home.tmpl",
 	}
 
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
-	// 用 template.ParseFiles() 方法来读取模板到 template set，如果报错就返回一个 500 错误
-	// 路径必须是基于工作目录的相对路径，或者是绝对路径
-	// 注意这里的参数与之前不同
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
 		log.Print(err.Error())
@@ -42,9 +31,6 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
-	// 用 ExecuteTemplate() 方法
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
 		log.Print(err.Error())
@@ -52,17 +38,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// 添加 snippetView 处理方法
-// Add a snippetView handler function.
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
-	// Extract the value of the id parameter from the query string and try to
-	// convert it to an integer using the strconv.Atoi() function. If it can't
-	// be converted to an integer, or the value is less than 1, we return a 404 page
-	// not found response.
 
-	// 通过 r.URL.Query().Get() 方法获取 id 参数
-	// 因为用户输入的内容是不可信的，这里使用 strconv.Atoi() 方法将 id 转换成 int 类型的数字
-	// 如果转换失败或者 id 小于1，则返回一个 404 页面
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -71,11 +48,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		return
 	}
-	// Use the fmt.Fprintf() function to interpolate the id value with our response
-	// and write it to the http.ResponseWriter.
-	// 使用 fmt.Fprintf() 方法将 id 添加到响应中
-	// 注意这里使用了一个全新的返回响应的方法，fmt.Fprintf()，其第一个参数为 io.Writer(), 这个参数是一个接口
-	// 因为 http.ResponseWriter() 实现了这个接口，所以将它当作参数传入没有问题
+
 	_, err = fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 	if err != nil {
 		return
@@ -86,14 +59,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 // Add a snippetCreate handler function.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
-		// 这里使用一个判断来限定请求方法，如果不是 POST 请求，则返回一个 405 状态码，并且返回一个“请求方法不允许”的提示。
-		// If it's not, use the w.WriteHeader() method to send a 405 status
-		// code and the w.Write() method to write a "Method Not Allowed"
-		// response body. We then return from the function so that the
-		// subsequent code is not executed.
 
-		// 如果需要发送一个状态码不是 200 的响应，上一个提交中的方法有一些繁琐，这里可以使用 http.Error() 进行简化
-		// http.Error() 会进行一系列操作，基本等价于之前进行的操作
 		w.Header().Set("Allow", "POST")
 
 		// 显式地声明状态码
@@ -101,17 +67,6 @@ func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// 当返回请求时，Go 会自动帮你填充以下响应头：Date、Content-Length、Content-Type
-	// Go 会主动猜测响应地类型，如果匹配不到，最终会设置为 Content-Type: application/octet-stream
-	//
-	//w.Header().Set("Content-Type", "application/json")
-	//w.Write([]byte(`{"name":"Alex"}`))
-
-	// 设置响应头的一些方法
-	// 请求头会自动规范化为第一个字母大写，其余字母小写的形式，如果不需要这种自动格式化的操作，可以使用如下声明
-	//w.Header()["X-XSS-Protection"] = []string{"1; mode=block"}
-	// Set a new cache-control header. If an existing "Cache-Control" header exists
-	// it will be overwritten.
 	w.Header().Set("Cache-Control", "public, max-age=31536000")
 
 	// In contrast, the Add() method appends a new "Cache-Control" header and can
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,40 +17,16 @@ type application struct {
 }
 
 func main() {
-	// Use log.New() to create a logger for writing information messages. This takes
-	// three parameters: the destination to write the logs to (os.Stdout), a string
-	// prefix for message (INFO followed by a tab), and flags to indicate what
-	// additional information to include (local date and time). Note that the flags
-	// are joined using the bitwise OR operator |.
-	// 使用 log.New() 方法创建一个日志记录器，需要传入三个参数：日志的输出方，日志前缀，附带的信息（日期和时间，使用"|"连接）
-	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 
-	// Create a logger for writing error messages in the same way, but use stderr as
-	// the destination and use the log.Lshortfile flag to include the relevant
-	// file name and line number.
-	// 创建一个错误日志记录器，以 stderr 作为输出方，
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
 	mux := http.NewServeMux()
 
-	// Define a new command-line flag with the name 'addr', a default value of ":4000"
-	// and some short help text explaining what the flag controls. The value of the
-	// flag will be stored in the addr variable at runtime.
-	// 设置一个新的命令行参数 addr，默认值为 ":4000"，同时添加简短的参数说明，在启动时这个参数将存到 addr 变量中
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
-	// Importantly, we use the flag.Parse() function to parse the command-line flag.
-	// This reads in the command-line flag value and assigns it to the addr
-	// variable. You need to call this *before* you use the addr variable
-	// otherwise it will always contain the default value of ":4000". If any errors are
-	// encountered during parsing the application will be terminated.
-	// 使用 flag.Parse() 方法来解析命令行参数
 	flag.Parse()
 
-	// Create a file server which serves files out of the "./ui/static" directory.
-	// Note that the path given to the http.Dir function is relative to the project
-	// directory root.
-	// 创建一个文件服务，指定 "./ui/static/" 为根目录
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 
 	// Initialize a new instance of our application struct, containing the
@@ -61,10 +37,6 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	// Use the mux.Handle() function to register the file server as the handler for
-	// all URL paths that start with "/static/". For matching paths, we strip the
-	// "/static" prefix before the request reaches the file server.
-	// 用 mux.Handle() 注册文件服务，以 /static/ 开头的路径都将路由到文件服务
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
 	mux.HandleFunc("/", app.home)
