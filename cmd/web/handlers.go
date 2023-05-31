25f9764c7d82e9642b94e9fe90907838ef2b60b9
Auraxc <abeats@live.com>
2023-05-31 10:40:44 +0800

    添加 helper.go，优化 handlers 中对错误的处理

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
@@ -26,15 +25,13 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Use the serverError() helper.
 		return
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err.Error())
-		http.Error(w, "Internal Server Error", 500)
+		app.serverError(w, err) // Use the serverError() helper.
 	}
 }
 
@@ -42,7 +39,7 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w) // Use the notFound() helper.
 		return
 	}
 	if err != nil {
@@ -58,36 +55,15 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 // 添加 snippetCreate 处理方法
 // Add a snippetCreate handler function.
 func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
-	if r.Method != "POST" {
-
-		w.Header().Set("Allow", "POST")
-
-		// 显式地声明状态码
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		app.clientError(w, http.StatusMethodNotAllowed) // Use the clientError() helper.
 		return
 	}
-
-	w.Header().Set("Cache-Control", "public, max-age=31536000")
-
-	// In contrast, the Add() method appends a new "Cache-Control" header and can
-	// be called multiple times.
-	w.Header().Add("Cache-Control", "public")
-	w.Header().Add("Cache-Control", "max-age=31536000")
-
-	// Delete all values for the "Cache-Control" header.
-	// Del 操作不会删除 Go 自动设置的响应头，如果需要删除，那么需要使用如下方法
-	// w.Header()["Date"] = nil
-	w.Header().Del("Cache-Control")
-
-	// Retrieve the first value for the "Cache-Control" header.
-	w.Header().Get("Cache-Control")
-
-	// Retrieve a slice of all values for the "Cache-Control" header.
-	w.Header().Values("Cache-Control")
 
 	_, err := w.Write([]byte("Create a new snippet..."))
 
 	if err != nil {
-		return
+		app.serverError(w, err) // Use the serverError() helper.
 	}
 }
