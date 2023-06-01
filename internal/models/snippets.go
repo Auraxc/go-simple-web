14731aba22d14637365982a3ecbcbc785021fca2
Auraxc <abeats@live.com>
2023-06-01 17:32:27 +0800

    添加 Latest() 方法，获取最新的10条 snippet

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -6,10 +6,6 @@ import (
 	"time"
 )
 
-// Snippet Define a Snippet type to hold the data for an individual snippet. Notice how
-// the fields of the struct correspond to the fields in our MySQL snippets
-// table?
-// 定义一个结构
 type Snippet struct {
 	ID      int
 	Title   string
@@ -23,81 +19,103 @@ type SnippetModel struct {
 	DB *sql.DB
 }
 
-// Insert This will insert a new snippet into the database.
 func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
-	// Write the SQL statement we want to execute. I've split it over two lines
-	// for readability (which is why it's surrounded with backquotes instead
-	// of normal double quotes).
-	// 增加可读性分为两行，需要使用反引号
 	stmt := `INSERT INTO snippets (title, content, created, expires)
     VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
 
-	// Use the Exec() method on the embedded connection pool to execute the
-	// statement. The first parameter is the SQL statement, followed by the
-	// title, content and expiry values for the placeholder parameters. This
-	// method returns a sql.Result type, which contains some basic
-	// information about what happened when the statement was executed.
-	// 执行 sql 语句会返回一些执行的基础信息
 	result, err := m.DB.Exec(stmt, title, content, expires)
 	if err != nil {
 		return 0, err
 	}
 
-	// Use the LastInsertId() method on the result to get the ID of our
-	// newly inserted record in the snippets table.
-	// 获取插入结果的 ID
 	id, err := result.LastInsertId()
 	if err != nil {
 		return 0, err
 	}
 
-	// The ID returned has the type int64, so we convert it to an int type
-	// before returning.
-	// 返回的 ID 类型是 int64，转换成 int 类型并返回
 	return int(id), nil
 }
 
-// Get This will return a specific snippet based on its id.
 func (m *SnippetModel) Get(id int) (*Snippet, error) {
-	// Write the SQL statement we want to execute. Again, I've split it over two
-	// lines for readability.
+
 	stmt := `SELECT id, title, content, created, expires FROM snippets
     WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-	// Use the QueryRow() method on the connection pool to execute our
-	// SQL statement, passing in the untrusted id variable as the value for the
-	// placeholder parameter. This returns a pointer to a sql.Row object which
-	// holds the result from the database.
 	row := m.DB.QueryRow(stmt, id)
-
-	// Initialize a pointer to a new zeroed Snippet struct.
-	// 初始化结构
 	s := &Snippet{}
 
-	// Use row.Scan() to copy the values from each field in sql.Row to the
-	// corresponding field in the Snippet struct. Notice that the arguments
-	// to row.Scan are *pointers* to the place you want to copy the data into,
-	// and the number of arguments must be exactly the same as the number of
-	// columns returned by your statement.
-	// 使用 row.Scan() 方法将 sqlRow 查出的每个字段的值复制到 Snippet 结构对应的字段中
 	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
 	if err != nil {
-		// If the query returns no rows, then row.Scan() will return a
-		// sql.ErrNoRows error. We use the errors.Is() function check for that
-		// error specifically, and return our own ErrNoRecord error
-		// instead (we'll create this in a moment).
 		if errors.Is(err, sql.ErrNoRows) {
 			return nil, ErrNoRecord
 		} else {
 			return nil, err
 		}
 	}
-
-	// If everything went OK then return the Snippet object.
 	return s, nil
 }
 
-// Latest This will return the 10 most recently created snippets.
 func (m *SnippetModel) Latest() ([]*Snippet, error) {
-	return nil, nil
+	// Write the SQL statement we want to execute.
+	// SQL 语句
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+    WHERE expires > UTC_TIMESTAMP() ORDER BY id DESC LIMIT 10`
+
+	// Use the Query() method on the connection pool to execute our
+	// SQL statement. This returns a sql.Rows resultset containing the result of
+	// our query.
+	// 用 Query() 方法执行语句，返回查询到的所有行
+	rows, err := m.DB.Query(stmt)
+	if err != nil {
+		return nil, err
+	}
+
+	// We defer rows.Close() to ensure the sql.Rows resultset is
+	// always properly closed before the Latest() method returns. This defer
+	// statement should come *after* you check for an error from the Query()
+	// method. Otherwise, if Query() returns an error, you'll get a panic
+	// trying to close a nil resultset.
+	// 调用 defer rows.Close() 保证获取 sql.Rows 在 Latest() 返回前正确调用
+	// 如果调用报错，也能保证 rows.Close() 被调用，关闭底层的数据库连接
+	// 如果数据库连接不能被正确关闭，那么连接池的连接将被耗尽，之后就无法连接数据库了
+	defer rows.Close()
+
+	// Initialize an empty slice to hold the Snippet structs.
+	// 初始化一个空的切片来保存 Snippet 结构体
+	snippets := []*Snippet{}
+
+	// Use rows.Next to iterate through the rows in the resultset. This
+	// prepares the first (and then each subsequent) row to be acted on by the
+	// rows.Scan() method. If iteration over all the rows completes then the
+	// resultset automatically closes itself and frees-up the underlying
+	// database connection.
+	for rows.Next() {
+		// Create a pointer to a new zeroed Snippet struct.
+		s := &Snippet{}
+		// Use rows.Scan() to copy the values from each field in the row to the
+		// new Snippet object that we created. Again, the arguments to row.Scan()
+		// must be pointers to the place you want to copy the data into, and the
+		// number of arguments must be exactly the same as the number of
+		// columns returned by your statement.
+		// 用 Scan() 方法从原始数据复制到 Snippet 结构体中
+		err = rows.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+		if err != nil {
+			return nil, err
+		}
+		// Append it to the slice of snippets.
+		// 添加到切片 snippets
+		snippets = append(snippets, s)
+	}
+
+	// When the rows.Next() loop has finished we call rows.Err() to retrieve any
+	// error that was encountered during the iteration. It's important to
+	// call this - don't assume that a successful iteration was completed
+	// over the whole resultset.
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	// If everything went OK then return the Snippets slice.
+	// 如果一切正常则返回切片 snippets
+	return snippets, nil
 }
