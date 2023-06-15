b819cf38c273e608f1597810642842f4f4c1f64a
Auraxc <abeats@live.com>
2023-06-15 17:35:44 +0800

    添加 middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -2,8 +2,10 @@ package main
 
 import "net/http"
 
-// The routes() method returns a servemux containing our application routes.
-func (app *application) routes() *http.ServeMux {
+// Update the signature for the routes() method so that it returns a
+// http.Handler instead of *http.ServeMux.
+// 修改 routes() 的签名，返回 http.Handler 替代 *http.ServeMux。
+func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -13,5 +15,9 @@ func (app *application) routes() *http.ServeMux {
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
-	return mux
+	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
+	// Because secureHeaders is just a function, and the function returns a
+	// http.Handler we don't need to do anything else.
+	// 将 mux 传入 secureHeaders，因为 secureHeaders 只是一个函数，它会返回一个 http.Handler，所以其它地方不需要修改
+	return app.logRequest(secureHeaders(mux))
 }
