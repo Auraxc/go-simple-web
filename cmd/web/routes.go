ac42856db0de5da12a500f4eff522c397b737714
Auraxc <abeats@live.com>
2023-06-19 17:26:17 +0800

    使用 justinas/alice 优化 middleware 调用链

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,9 @@
 package main
 
-import "net/http"
+import (
+	"github.com/justinas/alice"
+	"net/http"
+)
 
 // Update the signature for the routes() method so that it returns a
 // http.Handler instead of *http.ServeMux.
@@ -14,10 +17,11 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/", app.home)
 	mux.HandleFunc("/snippet/view", app.snippetView)
 	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	// 创建一个中间件的调用链
+	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
-	// Pass the servemux as the 'next' parameter to the secureHeaders middleware.
-	// Because secureHeaders is just a function, and the function returns a
-	// http.Handler we don't need to do anything else.
-	// 将 mux 传入 secureHeaders，因为 secureHeaders 只是一个函数，它会返回一个 http.Handler，所以其它地方不需要修改
-	return app.logRequest(secureHeaders(mux))
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
