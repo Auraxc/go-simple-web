7ec6b298eeb26c4a704a4ee6fc2ff74940d1fb81
Auraxc <abeats@live.com>
2023-06-29 17:31:01 +0800

    修改 routes，使用 "github.com/julienschmidt/httprouter" 替代默认的路由

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"github.com/julienschmidt/httprouter" // New import
 	"github.com/justinas/alice"
+
 	"net/http"
 )
 
@@ -9,19 +11,19 @@ import (
 // http.Handler instead of *http.ServeMux.
 // 修改 routes() 的签名，返回 http.Handler 替代 *http.ServeMux。
 func (app *application) routes() http.Handler {
-	mux := http.NewServeMux()
-
+	router := httprouter.New()
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodGet, "/", app.home)
+	router.HandlerFunc(http.MethodGet, "/snippet/view/:id", app.snippetView)
+	router.HandlerFunc(http.MethodGet, "/snippet/create", app.snippetCreate)
+	router.HandlerFunc(http.MethodPost, "/snippet/create", app.snippetCreatePost)
 	// Create a middleware chain containing our 'standard' middleware
 	// which will be used for every request our application receives.
 	// 创建一个中间件的调用链
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
 	// Return the 'standard' middleware chain followed by the servemux.
-	return standard.Then(mux)
+	return standard.Then(router)
 }
