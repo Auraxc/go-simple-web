bcad7112d819a8729ddfb751bb84f8fa4c4af411
Auraxc <abeats@live.com>
2023-07-11 17:27:53 +0800

    使用统一样式的 404 页面

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,6 +12,16 @@ import (
 // 修改 routes() 的签名，返回 http.Handler 替代 *http.ServeMux。
 func (app *application) routes() http.Handler {
 	router := httprouter.New()
+
+	// Create a handler function which wraps our notFound() helper, and then
+	// assign it as the custom handler for 404 Not Found responses. You can also
+	// set a custom handler for 405 Method Not Allowed responses by setting
+	// router.MethodNotAllowed in the same way too.
+	// 使用 app.notFound(w) 来统一 404 页面的相应
+	router.NotFound = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		app.notFound(w)
+	})
+
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	router.Handler(http.MethodGet, "/static/*filepath", http.StripPrefix("/static", fileServer))
 
